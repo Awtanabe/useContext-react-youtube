3d2379d3a4f75719fd14f17ee1c1ab4b1efff5be
Awtanabe <panser098@gmail.com>
2021-01-21 22:56:23 +0900

    Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import MovieList from './MovieList';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
+    <Nav/>
     <MovieList/>
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const Nav = (props) => {
+  return(
+    <div>
+      <h3>Akifumi </h3>
+      <p>list orf movies</p>
+    </div>
+  )
+}
+
+export default Nav;
